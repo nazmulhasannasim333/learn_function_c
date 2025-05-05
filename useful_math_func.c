f7576e22ae90d3653aa6ce89c435d30ac13ad77f
Nasim <mdnasimhosen333@gmail.com>
2025-05-05 22:05:43 +0600

    Enhance mathematical function outputs and improve result display

--- useful_math_func.c
+++ useful_math_func.c
@@ -4,6 +4,12 @@
 int main()
 {
     int ceil_result = ceil(5.3);
-    printf("%d\n", ceil_result);
+    int floor_result = floor(5.3);
+    int round_result = round(5.3);
+    int sqrt_result = sqrt(16);
+    int pow_result = pow(2, 3);
+    int abs_result = abs(-5);
+    printf("Ceil Result ==> %d\n", ceil_result);
+
     return 0;
 }
